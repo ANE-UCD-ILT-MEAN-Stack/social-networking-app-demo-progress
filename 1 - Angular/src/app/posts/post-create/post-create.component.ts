73664d3cc4e4cdda1b01faa703754b3f14864b52
BipulKumar <vip5sep@gmail.com>
2020-05-06 15:59:24 -0400

    Checking in Changes for Completed Angular App - First Module

--- 1 - Angular/src/app/posts/post-create/post-create.component.ts
+++ 1 - Angular/src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { PostsService } from '../post.service';
 
 @Component({
   selector: 'app-post-create',
@@ -6,16 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent implements OnInit {
-
-  constructor() { }
+  constructor( public postsService : PostsService) { }
 
   ngOnInit(): void {
   }
-  newPost='No Content';
-  enteredValue = '';
-
-  onAddPost(){
-    this.newPost = this.enteredValue;
+  enteredTitle = '';
+  enteredContent = '';
+  onAddPost(form: NgForm){   
+    if(form.invalid){
+      return;
+    }   
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
-
 }
