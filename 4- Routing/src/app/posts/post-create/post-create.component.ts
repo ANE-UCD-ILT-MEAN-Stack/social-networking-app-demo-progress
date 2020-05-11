65782f1f0cf6a1e350865e9184fea1ffa6ba43d1
Jathin Devaiah <9070486+jathindevaiah@users.noreply.github.com>
2020-05-11 11:34:40 -0400

    Adding routing code

---
+++ 4- Routing/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+
+import { NgForm } from '@angular/forms';
+import { PostsService } from '../posts.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Post } from '../post.model';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit{
+  enteredTitle = '';
+  enteredContent = '';
+  private mode = 'create';
+  private postId: string;
+  post: Post;
+  isLoading = false;
+
+  constructor(public postsService: PostsService, public route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if(paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.isLoading = true;
+        this.postsService.getPost(this.postId).subscribe(postData => {
+          this.isLoading = false
+          this.post = {id: postData._id, title: postData.title, content: postData.content }
+        });
+      }
+      else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
+  }
+
+  onSavePost(form: NgForm){
+    if(form.invalid) return;
+
+    this.isLoading = true;
+    if(this.mode === 'create') {
+      this.postsService.addPost(form.value.title, form.value.content);
+    }
+    else {
+      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+    }
+
+
+    form.resetForm();
+  }
+}
